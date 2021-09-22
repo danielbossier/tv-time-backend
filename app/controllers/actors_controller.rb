a0e250a167a2fccc3c04880894320c3cf26466aa
danielbossier <dbossier21@gmail.com>
2021-09-22 12:05:49 -0700

    added RESTful actions to actors controller

--- app/controllers/actors_controller.rb
+++ app/controllers/actors_controller.rb
@@ -1,2 +1,46 @@
 class ActorsController < ApplicationController
+  def index
+    actors = Actor.all
+    render json: actors.as_json
+  end
+
+  def create
+    actor = Actor.new(
+      title: params[:title],
+      year: params[:year],
+      description: params[:description],
+      creator: params[:creator],
+      seasons: params[:seasons],
+      network: params[:network],
+      favorite: params[:favorite],
+      user_id: params[:user_id],
+    )
+    actor.save
+    render json: actor.as_json
+  end
+
+  def show
+    actor = Actor.find_by(id: params[:id])
+    render json: actor.as_json
+  end
+
+  def update
+    actor = Actor.find_by(id: params[:id])
+    actor.title = params[:title] || actor.title
+    actor.year = params[:year] || actor.year
+    actor.description = params[:description] || actor.description
+    actor.creator = params[:creator] || actor.creator
+    actor.seasons = params[:seasons] || actor.seasons
+    actor.network = params[:network] || actor.network
+    actor.favorite = params[:favorite] || actor.favorite
+    actor.user_id = params[:user_id] || actor.user_id
+    actor.save
+    render json: show.as_json
+  end
+
+  def destroy
+    actor = Actor.find_by(id: params[:id])
+    actor.destroy
+    render json: { message: "Actor successfully destroyed." }
+  end
 end
